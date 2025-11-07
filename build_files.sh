cdc0c3d76e80da5f6e08fa67bde5c4d87722885d
Subhajit Mitra <143322078+SubhajitMitra-GH@users.noreply.github.com>
2025-11-07 19:32:36 +0530

    Update build script for ffmpeg installation

--- build_files.sh
+++ build_files.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-set -e # Exit immediately if a command exits with a non-zero status.
+set -e # Exit immediately if a command fails
 
-echo "Installing download tools..."
-# Install curl and tar/xz utils, which are needed to download and extract
-yum install -y curl tar xz-utils
+echo "Installing xz (for extracting .tar.xz)..."
+# 'xz-utils' wasn't found, try the more common package name 'xz'
+yum install -y xz
 
 echo "Downloading static ffmpeg build..."
-# Download a known-good static build of ffmpeg from a trusted source
 curl -L -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 
 echo "Extracting ffmpeg..."
-# Extract the tarball
+# Now that 'xz' is installed, tar's 'x' flag should work
 tar -xf ffmpeg.tar.xz
 
-# The tarball extracts to a folder like 'ffmpeg-6.0-amd64-static'
-# We use a wildcard * to find the ffmpeg binary inside that folder
 echo "Moving ffmpeg to /usr/local/bin..."
+# Use wildcard to find the folder
 mv ffmpeg-*-amd64-static/ffmpeg /usr/local/bin/ffmpeg
-
-# Also move ffprobe, which whisper sometimes uses
 mv ffmpeg-*-amd64-static/ffprobe /usr/local/bin/ffprobe
 
 echo "Cleaning up..."
-# Clean up the downloaded file and extracted folder
 rm -f ffmpeg.tar.xz
 rm -rf ffmpeg-*-amd64-static
 
